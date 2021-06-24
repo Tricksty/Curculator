fc99cce316f49762b248c59c07ea1aae3c309500
Зуев Руслан <refixd@mail.ru>
2021-06-24 23:45:17 +0300

    I played MK11 for 7 hours ;3, Nastya the best, thx for formules.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import json
 
+from math import log
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -17,15 +18,59 @@ def function_1(request):
 
     if request.method == 'POST':
         data = json.loads(request.body.decode("utf-8"))
-        # казалось бы это должно работать так
-        amount_at_start = float(data['amount_at_start'])
-        deposit_percent = float(data['deposit_percent'])
-        ages = float(data['ages'])
-        inflation_rate = float(data['inflation_rate'])
-        amount_at_end = (amount_at_start * (1 + deposit_percent / 100) ** ages) / (1 + inflation_rate / 100) ** ages
+
+        aas_bool = data['amount_at_start'].isdigit()
+        dp_bool = data['deposit_percent'].isdigit()
+        a_bool = data['ages'].isdigit()
+        ir_bool = data['inflation_rate'].isdigit()
+        aae_bool = data['amount_at_end'].isdigit()
+
+        amount_at_start = 0
+        deposit_percent = 0
+        ages = 0
+        inflation_rate = 0
+        amount_at_end = 0
+
+        if not aas_bool:
+            deposit_percent = float(data['deposit_percent'])
+            ages = float(data['ages'])
+            inflation_rate = float(data['inflation_rate'])
+            amount_at_end = float(data['amount_at_end'])
+            amount_at_start = amount_at_end * ((1 + inflation_rate / 100) ** ages) / ((1 + deposit_percent / 100) ** ages)
+        elif not dp_bool:
+            amount_at_start = float(data['amount_at_start'])
+            ages = float(data['ages'])
+            inflation_rate = float(data['inflation_rate'])
+            amount_at_end = float(data['amount_at_end'])
+            deposit_percent = ((amount_at_end * ((1 + inflation_rate / 100) ** ages) / amount_at_start) ** (1 / ages) - 1) * 100
+        elif not a_bool:
+            amount_at_start = float(data['amount_at_start'])
+            deposit_percent = float(data['deposit_percent'])
+            inflation_rate = float(data['inflation_rate'])
+            amount_at_end = float(data['amount_at_end'])
+            base = amount_at_start * (1 + deposit_percent / 100) / (1 + inflation_rate / 100)
+            ages = log(amount_at_end, base)
+        elif not ir_bool:
+            amount_at_start = float(data['amount_at_start'])
+            deposit_percent = float(data['deposit_percent'])
+            ages = float(data['ages'])
+            amount_at_end = float(data['amount_at_end'])
+            inflation_rate = ((amount_at_end / (amount_at_start * (1 + deposit_percent / 100) ** ages)) ** (1 / ages) - 1) * 100
+        elif not aae_bool:
+            amount_at_start = float(data['amount_at_start'])
+            deposit_percent = float(data['deposit_percent'])
+            ages = float(data['ages'])
+            inflation_rate = float(data['inflation_rate'])
+            amount_at_end = (amount_at_start * (1 + deposit_percent / 100) ** ages) / (1 + inflation_rate / 100) ** ages
         result = {
-            'amount_at_end': amount_at_end,
+            'amount_at_start': amount_at_start,
+            'deposit_percent': deposit_percent,
+            'inflation_rate': inflation_rate,
+            'ages': ages,
+            'amount_at_end': amount_at_end
         }
+
         return HttpResponse(json.dumps(result))
+
     elif request.method == 'GET':
-        return HttpResponse("Something get wrong")
+        return HttpResponse("There is nothing to GET ;3")
--- static/js/app/index.js
+++ static/js/app/index.js
@@ -1,14 +1,20 @@
-let p = document.getElementById('amount_at_end');
 let button = document.getElementById('submit');
 
 button.addEventListener('click', async (e) => {
     e.preventDefault();
 
+    let amountAtStart = document.getElementById('amount_at_start');
+    let depositPercent = document.getElementById('deposit_percent');
+    let ages = document.getElementById('ages');
+    let inflationRate = document.getElementById('inflation_rate');
+    let amountAtEnd = document.getElementById('amount_at_end');
+
     const res = await request({url: 'credit', body: {
-        amount_at_start: document.getElementById('amount_at_start').value,
-        deposit_percent: document.getElementById('deposit_percent').value,
-        ages: document.getElementById('ages').value,
-        inflation_rate: document.getElementById('inflation_rate').value,
+        amount_at_start: amountAtStart.value,
+        deposit_percent: depositPercent.value,
+        ages: ages.value,
+        inflation_rate: inflationRate.value,
+        amount_at_end: amountAtEnd.value,
     }, headers:{
         'X-CSRFToken': csrf_token,
     }});
@@ -16,5 +22,11 @@ button.addEventListener('click', async (e) => {
     console.log(res);
     console.log(JSON.parse(res));
 
-    p.innerHTML = JSON.parse(res).amount_at_end;
+    tmp = JSON.parse(res);
+
+    amountAtStart.value = tmp['amount_at_start'];
+    depositPercent.value = tmp['deposit_percent'];
+    ages.value = tmp['ages'];
+    inflationRate.value = tmp['inflation_rate'];
+    amountAtEnd.value = tmp['amount_at_end'];
 })
